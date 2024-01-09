2825726cd76b2d69c28b2d167b7b688044434bb4
SaiGargey <119662074+SaiGargey@users.noreply.github.com>
2024-01-09 15:00:30 +0530

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,21 +3,21 @@ import { Link } from "react-router-dom";
 
 export default function Home(){
     return (
-        <div className="parenth">
-            <div className="Home container m-0">
-            <div id='title' className="row ">
-                <div className ="display-1 col text-center">TIRES ON HIGHWAYS</div>                 
+        <div className="Home">
+            <div>
+                <p id='title'>TIRES ON HIGHWAYS</p>
             </div>
-            <div className="options row text-center">
-                <div className="option1 col-12 mt-2 mb-2">
-                <Link to="/guest"><button type="button"  className="btn btn-dark gs" draggable>Continue as Guest</button></Link>
+            <div className="options">
+                <div className="option1">
+                <Link to="/guest"><button type="button" className="btn btn-dark gs" draggable>Continue as Guest</button></Link>
                 </div>
                 {/* <br /> */}
-                <div className="option2 col-12 mt-2 mb-2">
+                <div className="option2">
                 <Link to="/toll/upload"><button type="button" className="btn btn-dark gs">Toll Sign In</button></Link>
-                
+                </div>
+                <div className="fixed-top">
+                <Link to="/contact"><button type="button" className="btn btn-dark gs" draggable>Contact Us</button></Link>
                 </div>
             </div>
-        </div>
         </div>
     );}
